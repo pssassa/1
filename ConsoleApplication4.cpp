b1fc53daa1edf2104e96b1da1366a873470df8f8
pssassa <95397262+pssassa@users.noreply.github.com>
2022-04-02 21:20:54 +0700

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void prog(istream& in) {
+
+    int count = 0;
+    string line;
+    while (getline(in, line))
+    {
+        int countA = 0;
+        int countE = 0;
+        int i = 0;
+        while (line[i]) {
+            if (line[i] == 'A')
+                countA++;
+            if (line[i] == 'E')
+                countE++;
+            i++;
+        }
+        if (countE > countA)
+            count++;
+    }
+    cout << "Result: " << count << endl;
+}
+  
+
+
+
+int main()
+{
+    ifstream in("text.txt");
+    if (!eof("text.txt") {
+        cout<<" file dont opened"
+    }
+    else {
+        prog(in);
+    }
+}
+
+        
